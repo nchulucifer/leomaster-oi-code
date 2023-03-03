4e7311361afa469fa594a662c4ea28597f66ff16
leomaster <38276862+nchulucifer@users.noreply.github.com>
2023-03-03 15:40:41 +0800

    Update ybt1028.cpp

--- ybt/ybt1028.cpp
+++ ybt/ybt1028.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-	char c;
-	cin >> c;
-	cout << "  " << c << endl;
-	cout << " " << c << c << c << endl;
-	cout << c << c << c << c << c << endl;
-	cout << " " << c << c << c << endl;
-	cout << "  " << c << endl;
-	return 0;
+    char c;
+    cin >> c;
+    cout << "  " << c << endl;
+    cout << " " << c << c << c << endl;
+    cout << c << c << c << c << c << endl;
+    cout << " " << c << c << c << endl;
+    cout << "  " << c << endl;
+    return 0;
 }
